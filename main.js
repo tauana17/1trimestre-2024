a9988254559e3e6b635750aff352ba939cea2bbe
tauana17 <104360790+tauana17@users.noreply.github.com>
2024-03-12 14:16:29 -0300

    11/03

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 for(let i=0; i < botoes.length; i++){
     botoes[i].onclick = function(){
 
-    for(let j=0; j<botoes.length; j++) {
+    for(let j=0; j<botoes.length; j++){
         botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo");
     }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
