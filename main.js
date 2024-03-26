91c753f6954d953e4df0da70eb5ad2d4e3dc7223
tauana17 <104360790+tauana17@users.noreply.github.com>
2024-03-26 14:19:36 -0300

    26/03

--- main.js
+++ main.js
@@ -16,7 +16,11 @@ for(let i=0; i < botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date ("2024-07-15T00:00:00");
+const tempoObjetivo3 = new Date ("2024-11-14T00:00:00");
+const tempoObjetivo4 = new Date ("2024-12-09T00:00:00");
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
@@ -33,3 +37,12 @@ function calculaTempo(tempoObjetivo){
     horas %= 24;
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
+
+function atualizaCronometro(){
+for(let i = 0; i <contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+   }
+}
+
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
