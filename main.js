f2db1b5accefec0bb6ed7a723951ab08c90aac0b
tauana17 <104360790+tauana17@users.noreply.github.com>
2024-04-23 14:09:12 -0300

    23/04

--- main.js
+++ main.js
@@ -44,12 +44,15 @@ function calculaTempo(tempoObjetivo){
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("seg1").textContent = calculaTempo(tempos[1])[1];
+    document.getElementById("seg2").textContent = calculaTempo(tempos[2])[2];
+    document.getElementById("seg3").textContent = calculaTempo(tempos[3])[3];
 for (let i = 0; i<contadores.length; i++){
-  //contadores[i].textContent = calculaTempo(tempos[i]);
+  document.getElementById("dias" +i).textContent = calculaTempo(tempos[i])[0];
+  document.getElementById("horas" +i).textContent = calculaTempo(tempos[i])[1];
+  document.getElementById("min" +i).textContent = calculaTempo(tempos[i])[2];
+  document.getElementById("seg" +i).textContent = calculaTempo(tempos[i])[3];
     }
 }
 
